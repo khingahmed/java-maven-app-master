eabdaa2ec34fb5915cfff1281296e350850c34e9
AhmedOkesina <ahmedokesina@yahoo.com>
2026-02-19 16:30:19 -0500

    moved all to git repo

--- java-maven-app-master/src/main/java/com/example/Application.java
+++ java-maven-app-master/src/main/java/com/example/Application.java
@@ -2,11 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args)
@@ -19,6 +22,11 @@ public class Application {
     {
         Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Java Maven Application";
     }
 
     public String getStatus() {
